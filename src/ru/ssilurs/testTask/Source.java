4e700adfce23ff50cdd89685128a63ccf8387505
SSiluRS <Endercresman397@mail.ru>
2023-07-27 19:20:08 +0700

    Refactoring

--- src/ru/ssilurs/testTask/Source.java
+++ src/ru/ssilurs/testTask/Source.java
@@ -5,44 +5,55 @@ import java.util.Map;
 
 public class Source {
     public static void main(String[] args) {
-        Map<String, String> yesterdayState = new HashMap<>();
-        Map<String, String> todayState = new HashMap<>();
+        Map<String, String> yesterdayState = new HashMap<>() {{
+            put("url1", "html1");
+            put("url2", "html2");
+            put("url3", "html3");
+            put("url", "html");
+        }};
 
-        yesterdayState.put("url1", "html1");
-        yesterdayState.put("url2", "html2");
-        yesterdayState.put("url3", "html3");
-        yesterdayState.put("url", "html");
+        Map<String, String> todayState = new HashMap<>() {{
+            put("url1", "html1");
+            put("url2", "hml2");
+            put("url3", "hml3");
+            put("url4", "html4");
+        }};
 
-        todayState.put("url1", "html1");
-        todayState.put("url2", "hml2");
-        todayState.put("url3", "hml3");
-        todayState.put("url4", "html4");
+        StringBuilder emailContent = new StringBuilder()
+                .append("Здравствуйте, дорогая и.о. секретаря\n\n")
+                .append("За последние сутки во вверенных Вам сайтах произошли следующие изменения:\n\n")
+                .append("Исчезли следующие страницы:\n");
 
-        StringBuilder emailContent = new StringBuilder();
-        emailContent.append("Здравствуйте, дорогая и.о. секретаря\n\n");
-        emailContent.append("За последние сутки во вверенных Вам сайтах произошли следующие изменения:\n\n");
-
-        emailContent.append("Исчезли следующие страницы:\n");
-        for (String url : yesterdayState.keySet()) {
-            if (!todayState.containsKey(url)) {
-                emailContent.append(url).append("\n");
-            }
-        }
+        yesterdayState
+                .keySet()
+                .stream()
+                .filter(s ->
+                        !todayState.containsKey(s))
+                .forEach(s ->
+                        emailContent.append(s).append("\n")
+                );
 
         emailContent.append("\nПоявились следующие новые страницы:\n");
-        for (String url : todayState.keySet()) {
-            if (!yesterdayState.containsKey(url)) {
-                emailContent.append(url).append("\n");
-            }
-        }
+        todayState
+                .keySet()
+                .stream()
+                .filter(url ->
+                        !yesterdayState.containsKey(url))
+                .forEach(url ->
+                        emailContent.append(url).append("\n")
+                );
 
         emailContent.append("\nИзменились следующие страницы:\n");
-        for (String url : yesterdayState.keySet()) {
-            if (todayState.containsKey(url) && !yesterdayState.get(url).equals(todayState.get(url))) {
-                emailContent.append(url).append("\n");
-            }
-        }
+        yesterdayState
+                .keySet()
+                .stream()
+                .filter(url ->
+                        todayState.containsKey(url) && !yesterdayState.get(url).equals(todayState.get(url))
+                )
+                .forEach(url ->
+                        emailContent.append(url).append("\n")
+                );
 
-        System.out.println(emailContent.toString());
+        System.out.println(emailContent);
     }
 }
